a57a836c10c8502c9a8ef4800c7a2fe867413e19
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-09-06 17:48:57 +0800

    CRUD users

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use App\Http\Requests\UserRequest;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['permission:users.index|users.create|users.edit|users.delete']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::latest()->when(request()->q,
+        function($users) {
+            $users = $users->where('name', 'like', '%' . request()->q . '%');
+        })->paginate(10);
+
+        return view('admin.user.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::latest()->get();
+        return view('admin.user.index',compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+
+        // assign role
+        $user->assignRole($request->input('role'));
+
+        if ($user) {
+            return redirect()->route('admin.user.index')->with(['success' => 'Data berhasil disimpan!']);
+        } else {
+            return redirect()->route('admin.user.index')->with(['error' => 'Data gagal disimpan!']);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $roles = Role::latest()->get();
+        return view('admin.user.edit', compact('user', 'roles'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Request\UserUpdateRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        $user = User::findOrFail($user->id);
+
+        if ($request->input('password') == "") {
+            $user->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email')
+            ]);
+        } else {
+            $user->update([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => bcrypt($request->input('password')),
+            ]);
+        }
+
+        // assign role
+        $user->syncRoles($request->input('role'));
+
+        if ($user) {
+            return redirect()->route('admin.user.index')->with(['success' => 'Data berhasil diupdate!']);
+        } else {
+            return redirect()->route('admin.user.index')->with(['error' => 'Data gagal diupdate!']);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id)->delete();
+
+        if ($user) {
+            return response()->json([
+                'status' => 'success'
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error'
+            ]);
+        }
+    }
+}
